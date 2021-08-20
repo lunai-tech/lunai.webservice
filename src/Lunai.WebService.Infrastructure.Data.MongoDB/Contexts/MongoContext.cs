74beeaa8495ba843c35954b5fc13a87605ca57fd
Marcelo Martins <martinsoliveira.marcelo@outlook.com>
2021-08-20 20:27:15 -0300

    Add MongoContext e ExpertRepository impl

---
+++ src/Lunai.WebService.Infrastructure.Data.MongoDB/Contexts/MongoContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lunai.WebService.Infrastructure.Data.MongoDB.Contexts
+{
+    public class MongoContext
+    {
+        private IMongoDatabase Database { get; set; }
+        
+        public MongoClient MongoClient { get; set; }
+        
+        private readonly List<Func<Task>> _commands;
+        
+        public IClientSessionHandle Session { get; set; }
+
+        public MongoContext(IConfiguration configuration)
+        {
+            // Every command will be stored and it'll be processed at SaveChanges
+            _commands = new List<Func<Task>>();
+
+            // Configure mongo (You can inject the config, just to simplify)
+            MongoClient = new MongoClient(Environment.GetEnvironmentVariable("MONGOCONNECTION") ?? configuration.GetSection("MongoSettings").GetSection("Connection").Value);
+
+            Database = MongoClient.GetDatabase(Environment.GetEnvironmentVariable("DATABASENAME") ?? configuration.GetSection("MongoSettings").GetSection("DatabaseName").Value);
+
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            using (Session = await MongoClient.StartSessionAsync())
+            {
+                Session.StartTransaction();
+
+                var commandTasks = _commands.Select(c => c());
+
+                await Task.WhenAll(commandTasks);
+
+                await Session.CommitTransactionAsync();
+            }
+
+            return _commands.Count;
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string name) => Database.GetCollection<T>(name);
+
+        public void Dispose()
+        {
+            Session?.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public void AddCommand(Func<Task> func) => _commands.Add(func);
+    }
+}
---
+++ src/Lunai.WebService.Infrastructure.Data.MongoDB/Repositories/ExpertRepository.cs
@@ -0,0 +1,36 @@
+﻿using Lunai.WebService.Domain.Entities.MongoDocuments.ExpertDocuments;
+using Lunai.WebService.Domain.Interfaces.Repositories;
+using Lunai.WebService.Infrastructure.Data.MongoDB.Contexts;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lunai.WebService.Infrastructure.Data.MongoDB.Repositories
+{
+    public class ExpertRepository : IExpertRepository
+    {
+        protected readonly MongoContext _context;
+        protected readonly IMongoCollection<ExpertDocument> DbSet;
+
+        protected ExpertRepository(MongoContext context)
+        {
+            _context = context;
+            DbSet = _context.GetCollection<ExpertDocument>("Experts");
+        }
+
+        public async Task<IEnumerable<ExpertDocument>> GetAll()
+        {
+            var data = await DbSet.FindAsync(Builders<ExpertDocument>.Filter.Empty);
+            return data.ToList();
+        }
+
+        public async Task<ExpertDocument> GetById(string idExpert)
+        {
+            var data = await DbSet.FindAsync(Builders<ExpertDocument>.Filter.Eq("_id", idExpert));
+            return data.FirstOrDefault();
+        }
+    }
+}

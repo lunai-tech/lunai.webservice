7d8d9658f4e663e84f322f81b880dc785969d0cc
Marcelo Martins <martinsoliveira.marcelo@outlook.com>
2021-08-20 19:59:14 -0300

    Add experts data

---
+++ src/Lunai.WebService.Domain/Entities/MongoDocuments/Address.cs
@@ -0,0 +1,30 @@
+﻿
+
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Lunai.WebService.Domain.Entities.MongoDocuments
+{
+    public class Address
+    {
+        [BsonElement("cep")]
+        public string Cep { get; set; }
+
+        [BsonElement("address")]
+        public string AddressValue { get; set; }
+
+        [BsonElement("number")]
+        public string Number { get; set; }
+
+        [BsonElement("district")]
+        public string District { get; set; }
+
+        [BsonElement("complement")]
+        public string Complement { get; set; }
+
+        [BsonElement("city")]
+        public string City { get; set; }
+
+        [BsonElement("state")]
+        public string State { get; set; }
+    }
+}
